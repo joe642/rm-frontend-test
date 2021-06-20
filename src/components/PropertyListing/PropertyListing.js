6c437e1b9be77d13c583ff8174c71a1443f56923
Joe Tomlinson <joe.tomlinson@blendology.com>
2021-06-20 20:30:43 +0100

    Fetch data from API using Axios, fixed failing test with mock data

--- src/components/PropertyListing/PropertyListing.js
+++ src/components/PropertyListing/PropertyListing.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import PropertyCard from '../PropertyCard';
 import './PropertyListing.scss';
 
-const DUMMY_PROPERTY = {
-    id: 73864112,
-    bedrooms: 3,
-    summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
-    displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
-    propertyType: 'Flat',
-    price: 1950000,
-    branchName: 'M2 Property, London',
-    propertyUrl: '/property-for-sale/property-73864112.html',
-    contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
-    propertyTitle: '3 bedroom flat for sale',
-    mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
-};
+const propertiesUrl = '/api/properties'
 
 const PropertyListing = () => {
+    const [properties, setProperties] = useState([]);
+    const getProperties = async () => {
+        const response = await axios.get(propertiesUrl, { params: {} });
+
+        setProperties(response.data);
+    };
+
+    useEffect(() => {
+        getProperties();
+    }, []);
+
     return (
         <div className="PropertyListing">
             {
-                Array(5).fill(DUMMY_PROPERTY)
+                properties
                     .map((property, index) => <PropertyCard key={index} {...property}/>)
             }
         </div>
--- src/components/PropertyListing/tests/PropertyListing.spec.js
+++ src/components/PropertyListing/tests/PropertyListing.spec.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import axios from 'axios'
+import { act } from "react-dom/test-utils";
+
 import PropertyListing from '../PropertyListing';
+
+jest.mock("axios");
 
 describe('PropertyListing', () => {
 
     it('should render without crashing', () => {
-        const wrapper = shallow(<PropertyListing/>);
+        const wrapper = shallow(<PropertyListing />);
         expect(wrapper.find('.PropertyListing')).toHaveLength(1);
     });
 
-    it('should render five property cards', () => {
-        const wrapper = shallow(<PropertyListing/>);
+    it('should render five property cards with mock data', async () => {
+        let wrapper;
+        const DUMMY_PROPERTY = {
+            id: 73864112,
+            bedrooms: 3,
+            summary: 'Property 1 Situated moments from the River Thames in Old Chelsea...',
+            displayAddress: '1 CHEYNE WALK, CHELSEA, SW3',
+            propertyType: 'Flat',
+            price: 1950000,
+            branchName: 'M2 Property, London',
+            propertyUrl: '/property-for-sale/property-73864112.html',
+            contactUrl: '/property-for-sale/contactBranch.html?propertyId=73864112',
+            propertyTitle: '3 bedroom flat for sale',
+            mainImage: 'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg'
+        };
+
+        await act(async () => {
+            const data = {
+                data: Array(5).fill(DUMMY_PROPERTY)
+            }
+
+            await axios.get.mockImplementationOnce(() => Promise.resolve(data));
+            wrapper = mount(<PropertyListing />);
+        });
+
+        wrapper.update();
+
+        await expect(axios.get).toHaveBeenCalledTimes(1);
         expect(wrapper.find('PropertyCard')).toHaveLength(5);
     });
 });
